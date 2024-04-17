07f4933ddaf4124ceb40fcfae78508c909dd403a
dthemesdevelopment <dthemesdevelopment@gmail.com>
2024-04-17 13:46:45 +0300

    Adding cloundary

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,22 @@
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {
+          folder: "Reef_Media",
+          use_filename: true,
+          unique_filename: false,
+        },
+        delete: {},
+      },
+    },
+  },
   'website-builder': {
 		enabled: true,
 		config: {
